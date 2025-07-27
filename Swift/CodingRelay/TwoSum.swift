44d0e803e8d8cf27663f6f6260c9f60a61a0a0d0
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-07-27 17:47:03 +0900

    Add TwoSum

---
+++ Swift/CodingRelay/TwoSum.swift
@@ -0,0 +1,38 @@
+//
+//  TwoSum.swift
+//  CodingRelay
+//
+//  Created by Akihiro Kokubo on 2025/07/27.
+//
+
+import Foundation
+
+/// 整数の配列 nums と整数 target が与えられます。
+/// 配列内の 2つの数のインデックス を返してください。その2つの数の合計が target になるようにしてください。
+/// ただし、各入力には必ず「ちょうど1つの解」が存在すると仮定してかまいません。同じ要素を2回使うことはできません。
+/// 答えはどちらの順番で返しても構いません。
+///
+/// 例 1:
+/// 入力: nums = [2,7,11,15], target = 9
+/// 出力: [0,1]
+/// 説明: nums[0] + nums[1] == 9 なので、[0, 1] を返します。
+///
+/// 例 2:
+/// 入力: nums = [3,2,4], target = 6
+/// 出力: [1,2]
+///
+/// 例 3:
+/// 入力: nums = [3,3], target = 6
+/// 出力: [0,1]
+///
+/// 制約:
+/// 2 <= nums.length <= 10⁴
+/// -10⁹ <= nums[i] <= 10⁹
+/// -10⁹ <= target <= 10⁹
+/// 答えはただ1つのみ存在します。
+
+enum TwoSum {
+    static func execute(_ x: Int) -> Int {
+        fatalError("SquareRoot.execute is unimplemented.")
+    }
+}
