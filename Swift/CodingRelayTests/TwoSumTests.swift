13775cd2c24bae644d2e4405fa9df9b81cbf8ca5
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-07-27 17:59:23 +0900

    Add test cases

--- Swift/CodingRelayTests/TwoSumTests.swift
+++ Swift/CodingRelayTests/TwoSumTests.swift
@@ -13,6 +13,10 @@ struct TwoSumTests {
         ([2, 7, 11, 15], 9, [0, 1]),
         ([3, 2, 4], 6, [1, 2]),
         ([3, 3], 6, [0, 1]),
+        ([7, 2, 11, 15], 9, [0, 1]),
+        ([0, 4, 3, 0], 0, [0, 3]),
+        ([-1, -2, -3, -4, -5], -8, [2, 4]),
+        ([Int.max, Int.min, 0], -1, [0, 1]),
     ])
     func execute(nums: [Int], target: Int, expected: [Int]) {
         let actual = TwoSum.execute(nums: nums, target: target)
