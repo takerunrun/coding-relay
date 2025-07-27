f4d759302192065fadc07c5293157dd29b90e03c
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-07-27 17:57:15 +0900

    Add TwoSumTests

---
+++ Swift/CodingRelayTests/TwoSumTests.swift
@@ -0,0 +1,21 @@
+//
+//  TwoSumTests.swift
+//  CodingRelayTests
+//
+//  Created by Akihiro Kokubo on 2025/07/27.
+//
+
+@testable import CodingRelay
+import Testing
+
+struct TwoSumTests {
+    @Test(arguments: [
+        ([2, 7, 11, 15], 9, [0, 1]),
+        ([3, 2, 4], 6, [1, 2]),
+        ([3, 3], 6, [0, 1]),
+    ])
+    func execute(nums: [Int], target: Int, expected: [Int]) {
+        let actual = TwoSum.execute(nums: nums, target: target)
+        #expect(actual.sorted() == expected)
+    }
+}
