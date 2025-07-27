57b94dd6f17c3fe8598c44f825e395d8fae2f7d9
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-07-27 17:19:18 +0900

    Add SquareRoot

---
+++ Swift/CodingRelay/SquareRoot.swift
@@ -0,0 +1,32 @@
+//
+//  SquareRoot.swift
+//  CodingRelay
+//
+//  Created by Akihiro Kokubo on 2025/07/27.
+//
+
+import Foundation
+
+/// 問題
+/// 非負の整数 x に対して、x の平方根を最も近い整数に切り捨てた値を返します。
+/// 返される整数も非負でなければなりません。
+///
+/// ⚠️注意
+/// 組み込みの指数関数や演算子を使用してはなりません。
+/// pow() や sqrt() を使ってはいけません。
+///
+/// 例 1:
+/// 入力: x = 4
+/// 出力: 2
+/// 説明: 4の平方根は2であるため、2を返します。
+///
+/// 例2:
+/// 入力: x = 8
+/// 出力: 2
+/// 説明: 8の平方根は2.82842...であり、最も近い整数に切り捨てたため、2が返されます。
+
+enum SquareRoot {
+    static func execute(_ x: Int) -> Int {
+        fatalError("SquareRoot.execute is unimplemented.")
+    }
+}
