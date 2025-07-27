eaa5e7dae19c67020524c0528c858598a096d4b9
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2025-07-27 17:04:40 +0900

    Add project files for Swift environment named CodingRelay

---
+++ Swift/CodingRelayTests/CodingRelayTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodingRelayTests.swift
+//  CodingRelayTests
+//
+//  Created by Akihiro Kokubo on 2025/07/27.
+//
+
+import Testing
+@testable import CodingRelay
+
+struct CodingRelayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
